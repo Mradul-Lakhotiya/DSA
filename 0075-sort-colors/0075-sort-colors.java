0a791921b1ac5d701f2d176e5bb4bff8d56580b8
Mradul Lakhotiya <142927820+Mradul-Lakhotiya@users.noreply.github.com>
2025-11-08 08:58:34 +0530

    Time: 0 ms (100%), Space: 43.3 MB (6.9%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] freq = new int[3];
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        int j = 0;
+        for (int i = 0; i < 3; i++) {
+            while (freq[i] != 0) {
+                nums[j] = i;
+                freq[i]--;
+                j++;
+            }
+        }
+    }
+}
