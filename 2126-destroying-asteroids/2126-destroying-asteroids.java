7505be3f4981a8cc33a69522a8391dc3a12f77ac
Mradul Lakhotiya <142927820+Mradul-Lakhotiya@users.noreply.github.com>
2026-05-31 11:32:26 +0530

    Time: 26 ms (81.52%), Space: 107.4 MB (47.81%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long m = mass;
+        
+        for (int x : asteroids) {
+            if (m < x) {
+                return false;
+            }
+            else {
+                m += x;
+            }
+        }
+
+        return true;
+    }
+}
