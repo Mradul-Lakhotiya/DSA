b4a3a726b0d91e2f02026a761a9f3d56425c10b3
Mradul Lakhotiya <142927820+Mradul-Lakhotiya@users.noreply.github.com>
2026-06-29 09:41:45 +0530

    Time: 3 ms (82.36%), Space: 43.3 MB (33.78%) - LeetHub

---
+++ 1507-reformat-date/1507-reformat-date.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public String reformatDate(String date) {
+        HashMap<String, String> map = new HashMap<>();
+
+        map.put("Jan", "01");
+        map.put("Feb", "02");
+        map.put("Mar", "03");
+        map.put("Apr", "04");
+        map.put("May", "05");
+        map.put("Jun", "06");
+        map.put("Jul", "07");
+        map.put("Aug", "08");
+        map.put("Sep", "09");
+        map.put("Oct", "10");
+        map.put("Nov", "11");
+        map.put("Dec", "12");
+
+        String[] parts = date.split(" ");
+
+        String day = parts[0];
+        String month = map.get(parts[1]);
+        String year = parts[2];
+
+        day = day.substring(0, day.length() - 2);
+
+        if (day.length() == 1) {
+            day = "0" + day;
+        }
+
+        return year + "-" + month + "-" + day;
+    }
+}
