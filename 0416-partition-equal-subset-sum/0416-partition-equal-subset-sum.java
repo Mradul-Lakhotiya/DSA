6009ffcb911920f30cd7cc091b9772d8cee36be9
Mradul Lakhotiya <142927820+Mradul-Lakhotiya@users.noreply.github.com>
2026-07-13 09:40:07 +0530

    Time: 337 ms (5.04%), Space: 75.8 MB (14.53%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,9 +1,8 @@
 class Solution {
     int n;
     int[] nums;
-    Boolean[][] dp;
+    boolean[][] dp;
     int totalSum = 0;
-    boolean flag = false;
 
     public boolean canPartition(int[] nums) {
         n = nums.length;
@@ -13,28 +12,50 @@ class Solution {
             totalSum += x;
         }
 
-        dp = new Boolean[n][2 * totalSum + 1];
-        return helper(0, 0);    
+        dp = new boolean[n + 1][2 * totalSum + 1];
+
+        dp[n][totalSum] = true;
+
+        // for (int sum = -totalSum; sum >= totalSum; sum++) {
+        //     dp[n][sum + toatlSum] = (sum == 0);
+        // }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int sum = -totalSum; sum <= totalSum; sum++) {
+                boolean takePlus = false;
+                boolean takeMinus = false;
+
+                int plus = sum + nums[i];
+                int minus = sum - nums[i];
+
+                if (plus <= totalSum) {
+                    takePlus = dp[i + 1][plus + totalSum];
+                }
+
+                if (minus >= -totalSum) {
+                    takeMinus = dp[i + 1][minus + totalSum];
+                }
+
+                dp[i][sum + totalSum] = takePlus || takeMinus;
+            }
+        }
+
+        return dp[0][totalSum];    
     }
 
-    boolean helper(int i, int sum) {
-        if (i == n && sum == 0) {
-            flag = true;
-            return true;
-        }
+    // boolean helper(int i, int sum) {
+    //     if (i == n && sum == 0) {
+    //         return true;
+    //     }
 
-        if (i == n) {
-            return false;
-        }
+    //     if (i == n) {
+    //         return false;
+    //     }
 
-        if (dp[i][sum + totalSum] != null) {
-            return dp[i][sum + totalSum];
-        }
+    //     if (dp[i][sum + totalSum] != null) {
+    //         return dp[i][sum + totalSum];
+    //     }
 
-        if (flag) {
-            return true;
-        }
-
-        return dp[i][sum + totalSum] = helper(i + 1, sum + nums[i]) || helper(i + 1, sum - nums[i]);
-    }
+    //     return dp[i][sum + totalSum] = helper(i + 1, sum + nums[i]) || helper(i + 1, sum - nums[i]);
+    // }
 }
