e6cff6cba30be25e07c394e2d810176f0ef2f9a1
Mradul Lakhotiya <142927820+Mradul-Lakhotiya@users.noreply.github.com>
2026-08-30 12:28:17 +0530

    Time: 0 ms (100%), Space: 42.2 MB (86.76%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,22 +1,9 @@
 class Solution {
     public int trailingZeroes(int n) {
-        int tow = 0;
-        int five = 0;
+        if (n < 5) {
+            return 0;
+        } 
 
-        for (int i = 1; i <= n; i++) {
-            int temp = i;
-
-            while (temp % 2 == 0) {
-                temp /= 2;
-                tow++;
-            }
-
-            while (temp % 5 == 0) {
-                temp /= 5;
-                five++;
-            }
-        }
-
-        return Math.min(tow, five);
+        return n / 5 + trailingZeroes(n / 5);
     }
 }
