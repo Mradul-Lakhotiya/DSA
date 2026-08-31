0d461298bf36518a6e13f2b08930d74e9ea3297f
Mradul Lakhotiya <142927820+Mradul-Lakhotiya@users.noreply.github.com>
2026-08-31 08:37:40 +0530

    Time: 4 ms (99.87%), Space: 106.2 MB (55.32%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -1,53 +1,45 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        List<Integer> arr = new ArrayList<>();
-
         if (head == null || head.next == null || head.next.next == null) {
             return new int[]{-1, -1};
         }
 
-
         ListNode prev = head;
         ListNode curr = head.next;
-        ListNode next = head.next.next;
-        int i = 1;
 
-        while (next != null) {
-            if (prev.val < curr.val && curr.val > next.val) {
-                arr.add(i);
+        int index = 1;
+
+        int first = -1;
+        int last = -1;
+        int min = Integer.MAX_VALUE;
+
+        while (curr.next != null) {
+            ListNode next = curr.next;
+
+            if ((curr.val > prev.val && curr.val > next.val) ||
+                (curr.val < prev.val && curr.val < next.val)) {
+
+                if (first == -1) {
+                    first = index;
+                } 
+                else {
+                    min = Math.min(min, index - last);
+                }
+
+                last = index;
             }
 
-            if (prev.val > curr.val && curr.val < next.val) {
-                arr.add(i);
-            }
-
-            i++;
-            prev = prev.next;
-            curr = curr.next;
-            next = next.next;
+            prev = curr;
+            curr = next;
+            index++;
         }
 
-        int min = Integer.MAX_VALUE;
-        int n = arr.size();
-
-        if (n < 2) {
+        if (first == last) {
             return new int[]{-1, -1};
         }
 
-        for (i = 1; i < n; i++) {
-            min = Math.min(min, arr.get(i) - arr.get(i - 1));
-        }
-        
-        return new int[]{min, arr.get(n - 1) - arr.get(0)};
+        int max = last - first;
+
+        return new int[]{min, max};
     }
 }
